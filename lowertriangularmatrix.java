0909b579d2e739574e108f7f863991503ab88368
Simran Arora <52946773+SimranArora123@users.noreply.github.com>
2021-11-18 15:31:39 +0530

    Add files via upload

---
+++ lowertriangularmatrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class lowertriangularmatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row= sc.nextInt();
+        int col=sc.nextInt();
+        int[][] arr=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]= sc.nextInt();
+            }
+        }
+        boolean isuppertriangular=true;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i][j]!=0){
+                    isuppertriangular=false;
+                    break;
+                }
+            }
+            if(!isuppertriangular){
+                break;
+            }
+        }
+        if(isuppertriangular){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
